8354ff82f3afccd87ba24f845eef1b9a6ce8fdb7
Sanip Shrestha <sanip_7@live.com>
2020-11-05 18:59:43 +1100

    Macaw Chart connecting to the data downloaded from the api

---
+++ Positron/UIViews/DailyStatTableView.swift
@@ -0,0 +1,21 @@
+//
+//  DailyStatTableView.swift
+//  Positron
+//
+//  Created by Sanip Shrestha on 11/2/20.
+//  Copyright © 2020 Sanip Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class DailyStatTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
